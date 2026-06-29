2eba096903e2c36ce12c2dadaa10a6eb9d810986
Matthew Graca <matthew.graca@gmail.com>
2026-06-29 14:28:32 -0700

    fixed erroneous gap filling and added corrupt timestamp removal. closes #4

--- src/gofer/composite.py
+++ src/gofer/composite.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import xarray as xr
+import numpy as np
 
 def composite(
     west_ds: xr.Dataset,
@@ -19,6 +20,9 @@ def composite(
     of hourly dates. The times of the Datasets will be normalized to the 
     nearest hour, then aligned and imputed according to those dates.
 
+    Any empty gaps in timestep (due to corrupted timestep or satellite gaps) 
+    will be filled with zeros.
+
     Arguments:
         west_ds: The GOES-West Dataset.
         east_ds: The GOES-East Dataset.
@@ -32,10 +36,24 @@ def composite(
     west_ds = west_ds.assign_coords(time=west_ds.time.dt.round("1h"))
     east_ds = east_ds.assign_coords(time=east_ds.time.dt.round("1h"))
 
-    # reindex to target time
+    # prune invalid timesteps due to improper storage on goes's end
     target_time = xr.DataArray(dates, dims="time", name="time")
-    west_ds = west_ds.reindex(time=target_time, fill_value=0.0)
-    east_ds = east_ds.reindex(time=target_time, fill_value=0.0)
+
+    west_valid_dates = west_ds.time.isin(dates)
+    east_valid_dates = east_ds.time.isin(dates)
+
+    west_ds = west_ds.sel(time=west_valid_dates)
+    east_ds = east_ds.sel(time=east_valid_dates)
+
+    # reindex to target time
+    west_ds = west_ds.reindex(
+        time=target_time,
+        fill_value={data_var: np.float32(0)}
+    )
+    east_ds = east_ds.reindex(
+        time=target_time,
+        fill_value={data_var: np.float32(0)}
+    )
 
     # concat
     combined_ds = xr.concat(
@@ -51,7 +69,7 @@ def composite(
             "latitude": combined_ds["latitude"],
             "longitude": combined_ds["longitude"],
         },
-        name="MaskConfidence",
+        name=data_var,
         attrs={
             "long_name": "Composite GOES West and East fire mask confidence"
         }
@@ -59,5 +77,6 @@ def composite(
 
     # merge on minimum value
     combined_ds = combined_ds.min(dim="satellite", skipna=True)
+    #combined_ds = combined_ds.max(dim="satellite", skipna=True)
 
     return combined_ds
