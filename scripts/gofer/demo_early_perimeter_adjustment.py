84c67c53969963bbe8013ec652319733e0425970
Matthew Graca <matthew.graca@gmail.com>
2026-07-07 17:02:43 -0700

    attempt #7 -- early perimeter adjustment seems to have 1.0 for any detections, will also need to cover edge cases

---
+++ scripts/gofer/demo_early_perimeter_adjustment.py
@@ -0,0 +1,31 @@
+from gofer.early_perimeter_adjustment import *
+import xarray as xr
+import pickle
+
+with open('temp/metadata.pkl', 'rb') as f:
+    data = pickle.load(f)
+    dates = data['dates']
+
+    buffer = 0.1
+    bbox = (
+        data['lon_min'] - buffer,
+        data['lat_min'] - buffer,
+        data['lon_max'] + buffer,
+        data['lat_max'] + buffer
+    )
+
+    dem_filepath = (
+        '/home/mgraca/Workspace/fire-spread/data/dem/'
+        'SRTMGL3_NC.003_SRTMGL3_DEM_doy2000042000000_aid0001.tif'
+    )
+
+# start after aggregate()
+ds = xr.open_dataset('temp/west_bobcat_2020_aggregated.nc', chunks={"time": 1})
+print(ds)
+
+ortho_kwargs = {
+    'dem_filepath' : dem_filepath,
+    'bbox' : bbox
+}
+ds = get_scaling_factors(ds, ortho_kwargs=ortho_kwargs)
+print(ds)
--- src/gofer/early_perimeter_adjustment.py
+++ src/gofer/early_perimeter_adjustment.py
@@ -5,8 +5,50 @@ For each hour h:
 
     Gather the confidences from hour [1, h]
     Calculate the cumulative max of the confidences from [1, h]
+    Parallax adjustment
     Apply spatial smoothing
     Calculate the max confidence of the entire frame s_h; the scaling factor (+ exceptions like t >= 500)
     For the entire frame, divide each pixel by s_h
 
 """
+from typing import Any
+
+import xarray as xr
+import numpy as np
+from gofer.spatial_smoothing import smooth
+from gofer.ortho import *
+from gofer.composite import dynamic_chunk
+from tqdm import tqdm
+
+
+def get_scaling_factors(
+    ds: xr.Dataset,
+    ortho_kwargs: dict[str, Any],
+    data_var: str = 'MaskConfidence',
+    show_progress: bool = True
+) -> xr.Dataset:
+    da = ds[data_var]
+    times = da['time'].values
+    running = None
+    scaling_factors = []
+    ortho_map = make_ortho_map(ds, **ortho_kwargs)
+    for i, t in enumerate(tqdm(times)) if show_progress else enumerate(times):
+        frame = da.isel({'time': i}).astype('float32').load()
+
+        if running is None:
+            running = frame.copy(deep=True)
+        else:
+            running = xr.apply_ufunc(np.fmax, running, frame, dask='allowed')
+
+        temp_ds = ds.isel({'time' : i}).load()
+        temp_ds[data_var] = running
+
+        temp_ds = apply_ortho_map(temp_ds, ortho_map, data_vars=[data_var])
+        temp_ds = smooth(temp_ds, input_variable=data_var)
+
+        s_t = temp_ds[data_var].max(dim=('latitude', 'longitude'), skipna=True).item()
+
+        scaling_factors.append(s_t)
+
+    return scaling_factors
+
